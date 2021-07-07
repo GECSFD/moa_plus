14e14ecda66d11b54c45a98071720a726cc77b6c
IgorFroehner <igoor.sf14@gmail.com>
2021-07-07 13:39:33 -0300

    result with args

---
+++ results/argument_parser.py
@@ -0,0 +1,82 @@
+
+import argparse
+
+parser = argparse.ArgumentParser()
+
+# task params
+parser.add_argument(
+    "--max_instances",
+    "-m",
+    type=int,
+    default=200000,
+    help="maximo de instancias por modelo"
+)
+parser.add_argument(
+    "--sample_frequency",
+    "-s",
+    type=int,
+    default=100000,
+    help="frequencia de retirada de informacoes"
+)
+
+# stream generator params
+parser.add_argument(
+    "--n_classes",
+    "-n",
+    type=int,
+    default=2,
+    help="numero de classes no stream"
+)
+parser.add_argument(
+    "--n_nominals",
+    "-nm",
+    type=int,
+    default=5,
+    help="numero de classes nominais"
+)
+parser.add_argument(
+    "--n_numericals",
+    "-nr",
+    type=int,
+    default=5,
+    help="numero de classes numericas"
+)
+
+# model params
+parser.add_argument(
+    "--remove_chance",
+    "-r",
+    type=float,
+    default=0.1,
+    help="chance de remocao"
+)
+parser.add_argument(
+    "--remove_classes",
+    "-rc",
+    type=bool,
+    default=True,
+    help="True para remover aleatoriamente algumas classes e False para não"
+)
+
+# remove chances
+parser.add_argument(
+    "--remove_chance_min",
+    "-rmin",
+    type=float,
+    default=0,
+    help="Minimo de chance para o remove chances"
+)
+parser.add_argument(
+    "--remove_chance_max",
+    "-rmax",
+    type=float,
+    default=1,
+    help="Máximo de chance para o remove chances"
+)
+parser.add_argument(
+    "--remove_chance_inc",
+    "-rinc",
+    type=float,
+    default=0.1,
+    help="Incremento do remove chances"
+)
---
+++ results/result_extractor.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import os
+
+from argument_parser import parser
+
+jar_name = 'moa-2020.07.2-SNAPSHOT.jar'
+jar_path = '../moa/target/'
+dump_path = './outs/'
+dump_name = 'out.csv'
+result_file_name = 'result.txt'
+
+if not os.path.exists(dump_path):
+    os.makedirs(dump_path)
+
+if not os.path.exists(f'{jar_path}{jar_name}'):
+    print('Please compile MOA before runing this')
+    print('Use: ')
+    print('\tmvn clean package') 
+    print('on project root folder')
+
+remove_chances = ['0.1']
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    remove_chance = args.remove_chance
+    remove_classes = args.remove_classes
+
+    n_classes = args.n_classes
+    n_nominals = args.n_nominals
+    n_numericals = args.n_numericals
+
+    max_instances = args.max_instances
+    sample_frequency = args.sample_frequency
+
+    remove_chance_min = args.remove_chance_min
+    remove_chance_max = args.remove_chance_max
+    remove_chance_inc = args.remove_chance_inc
+    assert(remove_chance_min < remove_chance_max)
+
+    remove_chances = []
+    remove_chance_item = remove_chance_min
+    while remove_chance_item < remove_chance_max:
+        remove_chances.append(remove_chance_item)
+        remove_chance_item = round(remove_chance_item+remove_chance_inc, 2)
+
+    open(result_file_name, 'w').close() # clean the results file before
+
+    for i in range(len(remove_chances)):
+        print(f'Running {i}')
+
+        dump_name = 'out'+str(i)+'.csv'
+        remove_chance = remove_chances[i]
+
+        open(f'{dump_path}{dump_name}', 'w').close() # clean the output file before running again
+
+        os.system(f'java -cp {jar_path}{jar_name} moa.DoTask "' +
+                f'EvaluatePrequential ' + # task choosen
+                f'-l (trees.SSLHoeffdingAdaptiveTree -C {remove_chance} -R {remove_classes}) ' + # class of selected model to be evaluated
+                f'-s (generators.RandomTreeGenerator -c {n_classes} -o {n_nominals} -u {n_numericals}) ' + # generator of the stream
+                f'-i {max_instances} ' + # max instances to classify
+                f'-d {dump_path}{dump_name} -f {sample_frequency}' + # dump options to dump
+                f'"')
+
+        csv_result = pd.read_csv(f'{dump_path}{dump_name}').tail(1) # picks only the last one
+
+        with open('result.txt', 'a') as result_file:
+            result_file.write(f'Running {i}, with remove_chance = {remove_chances[i]}:\n')
+            result_file.write(f'\tnumber of instances: {csv_result["classified instances"].item()}\n')
+            result_file.write(f'\tevaluation time (cpu seconds) {csv_result["evaluation time (cpu seconds)"].item()}\n')
+            result_file.write(f'\tclassified corect: {csv_result["classifications correct (percent)"].item()}\n\n')
+
